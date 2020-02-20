8d7e9e8864f3659e8cddbf78e052e8470201168e
jarpar <Mamotoja88>
2020-02-20 20:04:12 +0100

    refactored

--- src/metodydomyslne/Main.java
+++ src/metodydomyslne/Main.java
@@ -1,5 +1,15 @@
 package metodydomyslne;
 
 public class Main {
-
+    public static void main(String[] args) {
+    }
 }
+
+interface Formula {
+    double calculate(int a);
+
+    default double sqrt(int a) {
+        return Math.sqrt(a);
+    }
+}
+
